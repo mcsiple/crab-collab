362c9e93a8bda676adfc92e03469ad65e862a2f0
Megsie Siple <mcsiple@gmail.com>
2020-01-17 17:31:58 -0800

    Size-based, initial commit

---
+++ SizeModel.R
@@ -0,0 +1,22 @@
+# size-structure 1
+
+nages <- 3
+M <- 0.2   # natural mortality
+
+#Growth-transition matrix (females)
+G <- matrix(c(0.05,0.9,0,
+              0,0.1,0.9,
+              0,0,1),
+            nrow=nages)
+
+# Growth and survival matrix
+A <- G%*%exp(-M * diag(nages))
+
+# Nums at age in present year
+Nfem <- c(1,4,3) 
+
+# Recruitment of females and nums at age next year
+FemRecNext <- 100
+NfemNext <- Nfem%*%A 
+NfemNext[1] <- NfemNext[1] + FemRecNext
+NfemNext
