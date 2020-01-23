e064e6e9460a2ddc9993cb736628e03192369948
Megsie Siple <mcsiple@gmail.com>
2020-01-23 11:13:15 -0800

    Get size bins

--- get_size_matrix.R
+++ get_size_matrix.R
@@ -1,4 +1,11 @@
 # setup size matrix
-# Adapted from Maia Kapur's code for Kona crab
+dat <- read.csv(here::here('Heeia_crabdata_Apr2019.csv'))
+(minsize <- min(dat$CWid_LR_mm, na.rm=T))
+(maxsize <- max(dat$CWid_LR_mm, na.rm=T))
+
+# Set up size bins
+up_pts <- seq(minsize+4,maxsize,by=20)
+mid_pts <- up_pts-10
+
 
 # Generate size transition probability matrix following Siddeek et al (2016)
