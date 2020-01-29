2d171b7e8bdc427c16a7d280401406bbddd7e155
Maia Sosa Kapur <kapurm@uw.edu>
2020-01-29 15:26:35 -0800

    fix weird formatting stuff from PDF

--- Maia code/miscFunctions.R
+++ Maia code/miscFunctions.R
@@ -1,18 +1,11 @@
 ## SOURCE FUNCTIONS FOR KONA CRAB DEMOGRAPHIC ANALYSIS
 ##M KAPUR APR 2017
-## path edits from Siple in Jan 2020
-
 library(ggplot2)
 library(Rmisc)
-library(here)
-
 ## FUNCTIONS TO ESTIMATE DEMOGRAPHIC PARAMETERS TO INPUT TO LESLIE MATRIX
-
-source(here("Maia code","sizeMatrix.R")) ## generate the size matrix separately
-
+source('sizeMatrix.R') ## generate the size matrix separately
 ## FUNCTION ESTIMATE.AGE - INTERNAL USE ONLY
 ## PURPOSE: Generate a rough estimate of size at age for internal use only
-
 estimate.age = function(size.bins, Linf, K, tZero, longevDraw){
   age.est.vec = NULL
   age.est.vec[ncol(size.bins)] = longevDraw ## the last value is always the longevity
@@ -117,7 +110,7 @@ runBissect = function(Ulow,Uhigh,bissectIters,bissectConv,LeslieMat,tc)
 # Purpose: save list "OutLeslieMC" to txt files
 writeOutMC <- function(OutLeslieMC, Name)
 {
-  Path <- "outputs/"
+  Path <- "./outputs/"
   line = as.character(date()) ## include a timestamp
   ## special internal function for init list
   fnlist = function(x,fil){
@@ -352,7 +345,7 @@ savePlots = function(plotfunc, OutLeslieMC, Name, vals, form, height, width){
 ## function HARVESTPLOT
 ## purpose: plot misty mountains based on different H and TC
 harvestPlot = function(OutLeslieMC.FILE, Name, form){
-  Path = "figures/"
+  Path = "./figures/"
   if(form == 'png'){
     png(paste0(Path, Name,"_RvsHTC.",form), height = 960, width = 1200)
   } else if(form == 'eps'){
@@ -373,7 +366,7 @@ harvestPlot = function(OutLeslieMC.FILE, Name, form){
   par(oma = c(4, 4, 0, 0)) # make room (i.e. the 4's) for the overall x and y axis titles
   par(mar = c(1, 1, 2, 2)) # make the plots be closer together
   for (h in 1:length(h.vector)) {
-    plot(1, type = "n",xlim = c(-1.5, 1.5),ylim = c(0, 8.5),frame.plot = F,
+    plot(1, type = "n",xlim = c(-5, 5),frame.plot = F,
          main = paste0('Harvest Mortality = ', h.vector[h]), cex.main = 2,axes = F ) ## empty plot
     axis(side = 1,
          labels = (h == 9 | h == 10),
@@ -434,7 +427,7 @@ makeParStats = function(OutLeslieMC, tc = 0, Name, write.file){
     StatsParamsMC[,i] = c(meanVal,medianVal,CI95L,CI95U,n)
   }
   if(write.file == T){
-    Path <- "outputs/"
+    Path <- "./outputs/"
     write.table(StatsParamsMC, paste(Path, Name, "_StatsParamsMC.txt", sep=''), sep=",", row.names=F)
   }
   return(StatsParamsMC)
@@ -462,7 +455,7 @@ makeSADStats = function(OutLeslieMC, tc, Name, write.file){
     StatsSADMC[,i] = c(meanVal,medianVal,CI95L,CI95U,n)
   }
   if(write.file == T){
-    Path <- "outputs/"
+    Path <- "./outputs/"
     write.table(StatsSADMC, paste(Path, Name, "_StatsSADMC.txt", sep=''), sep=",", row.names=F)
   }
   return(StatsSADMC)
@@ -490,7 +483,7 @@ makeElastStats = function(OutLeslieMC, Name, write.file){
     StatsElastMC[,i] = c(meanVal,medianVal,CI95L,CI95U,n)
   }
   if(write.file == T){
-    Path <- "outputs/"
+    Path <- "./outputs/"
     write.table(StatsElastMC, paste(Path, Name, "_StatsElastMC.txt", sep=''), sep=",", row.names=F)
   }
   return(StatsElastMC)
@@ -516,7 +509,7 @@ makeElastStats = function(OutLeslieMC, Name, write.file){
     StatsElastMC[,i] = c(meanVal,medianVal,CI95L,CI95U,n)
   }
   if(write.file == T){
-    Path <- "outputs/"
+    Path <- "./outputs/"
     write.table(StatsElastMC, paste(Path, Name, "_StatsElastMC.txt", sep=''), sep=",", row.names=F)
   }
   return(StatsElastMC)
@@ -539,7 +532,7 @@ makeUsStats = function(OutLeslieMC, Name, write.file){
                                                                         'maxVal' = max(Us) ,
                                                                         'n' = n()))
   if(write.file == T){
-    Path <- "outputs/"
+    Path <- "./outputs/"
     write.table(StatsUsMC, paste(Path, Name, "_StatsUsMC.txt", sep=''), sep=",", row.names=F)
   }
   return(StatsUsMC)
