6c77e41bbbb496feb44f41f84f0c513cc1359a33
Megsie Siple <mcsiple@gmail.com>
2020-01-29 22:58:31 -0800

    revised params to those for S. serrata (from literature)

--- Maia code/paramsScenarios.R
+++ Maia code/paramsScenarios.R
@@ -1,31 +1,37 @@
 ## CENTRAL PARAMS FILE FOR KONA CRAB DEMOGRAPHY
 ## delete env
-rm(list = ls())
+# rm(list = ls())
 ## FIXED POINT ESTIMATES
 ## GROWTH ##
-Linf = c(115,120)[1] ## Australia, Dichmont & Brown (2010)
-K = c(0.26,0.45)[1] ## Australia, Dichmont & Brown (2010)
+Linf = 211  # S. serrata, Indonesia, La Sara (2010) (most recent available estimate)
+K = c(1.38,0.83)[1] ## S. serrata, Indonesia, La Sara (2010) 
 tZero = -0.1 ## Australia, Dichmont & Brown (2010)
-# Linf = 121.7 ## Australia, Kirkwood (2004)
-# K = 0.29 ## Australia, Kirkwood (2004)
-# tZero = -0.24 ## Australia, Kirkwood (2004)
+
 ## longevity parameters
 # maxage = 16 ## O'Neill
 # longevDraw = maxage ## just for syntax
+
+
 ## Growth Increment for Logit(Pmolt); derived from vonB; Chen & Kennelly (1999)
 bchen = exp(-K) - 1
 achen = -1 * bchen * Linf
 sdmolt = 10 ## assumes continuous variance in molt increment
+
+
 ## FECUNDITY ##
-L50 = 60 ## Hawaii, Onizuka minimum age
-## slope of length-fecundity equation (Onizuka 1972, 1000 eggs per mm)
-beta = 2081.8
-## fixed sex proportion of females - Onizuka
-SR = 0.45
+L50 = 90 ## this study 
+
+## slope of length-fecundity equation 
+## For Samoan crab, 15.55 x 1000 eggs increase per mm of carapace width - Sarower et al. (2013), Bangladesh - check w Maia ( 1000 eggs per mm is Kona crab value beta = 2081.8; from Onizuka 1972)
+beta = 15.55 
+## fixed sex proportion of females - fixed by me (Megsie) because we don't know the sex ratio...
+SR = 0.5
+
 ## MORTALITY ##
 ## hoenig (1983) "all species" values - for estimating mortality
 hoenig.slope = -0.982
 hoenig.int = 1.44
+
 ## HARVEST RATE POLICIES ##
 ## These aren't really used, have been replaced by decrement
 # harvConst = 0.3
@@ -34,18 +40,21 @@ hoenig.int = 1.44
 harvest.breaks = 10 ## number of even breaks from 0 - 0.9 to simulate harvest mortality (can't do 100%, gives -Inf
 # for rVal)
 hmin = 0
-hmax = 0.9
+hmax = 0.9 # max harvest rate
 h.vector = c(seq(hmin,hmax,1/harvest.breaks))
 ## Parameters for BISSECTION METHOD to estimate the stationary harvest, Hstat
 # Define the starting h values (for function runBissect)
 Uhigh <- .9 # High harvest rate to result in an eigenDom < 1
 Ulow <- 0 # Low harvest rate to result in an eigenDom >1
+
 # Define number of iterations in the Bissection method
 bissectIters <- 200
 # Define convergence level in the Bissection method
 bissectConv <- 0.00001
-## VECTOR OF TIME AT CAPTURE CUTOFFS
-tc.vector = c(0,35,L50,102) ## all ages, smallest observed catch, L50, legal size
+
+## VECTOR OF SIZE AT CAPTURE CUTOFFS
+tc.vector = c(0,76,L50,152) ## all ages, smallest observed catch (this study), L50, legal size for Samoan crab from HI state (6 in = 152.4 mm)
+
 ## make INITS object to save later
 inits = list( 'TIMESTAMP' = as.character(date()),
               'Linf' = Linf,
